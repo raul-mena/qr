61d5527dee76eef658bf8dcb590c6a2f55657956
raul-mena <raul-mena5@hotmail.com>
2020-09-22 16:37:10 -0500

    fix plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
-import { AlertController } from '@ionic/angular';
+import { AlertController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,63 +8,44 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  title = 'app';
-  elementType = 'url';
-  value = 'Techiediaries';
-  isScanning = false;
+  scanSub: any;
+  qrText: string;
+
   constructor(
-    private qrScanner: QRScanner,
-    private alertCtrl: AlertController
-  ) {}
-
-  changeValue(ev) {
-    this.value = ev.detail.value;
-  }
-
-  activeQrScanner() {
-    // Optionally request the permission early
-    this.qrScanner.prepare()
-    .then((status: QRScannerStatus) => {
-      if (status.authorized) {
-        this.isScanning = true;
-        // camera permission was granted
-        // start scanning
-        this.qrScanner.scan().subscribe((text: string) => {
-          console.log('Scanned something', text);
-          this.presentAlert(text);
-          setTimeout(() => {
-            this.stop(); // hide camera preview
-          }, 100);
-        });
-        this.qrScanner.show();
-      } else if (status.denied) {
-        this.presentAlert('camera permission was permanently denied');
-        // camera permission was permanently denied
-        // you must use QRScanner.openSettings() method to guide the user to the settings page
-        // then they can grant the permission from there
-      } else {
-        this.presentAlert('permission was denied, but not permanently');
-        // permission was denied, but not permanently. You can ask for permission again at a later time.
-      }
-    })
-    .catch((e: any) => console.log('Error is', e));
-  }
-
-  stop(){
-    this.qrScanner.hide(); // hide camera preview
-    this.qrScanner.destroy(); // hide camera preview
-    this.isScanning = false;
-  }
-
-  async presentAlert(msg) {
-    const alert = await this.alertCtrl.create({
-      cssClass: 'my-custom-class',
-      header: 'Alert',
-      subHeader: 'QR response',
-      message: msg,
-      buttons: ['OK']
+    public platform: Platform,
+    private qrScanner: QRScanner
+  ) {
+    this.platform.backButton.subscribeWithPriority(0, () => {
+      document.getElementsByTagName('body')[0].style.opacity = '1';
+      this.scanSub.unsubscribe();
     });
-
-    await alert.present();
   }
+
+  startScanning() {
+    // Optionally request the permission early
+    this.qrScanner.prepare().
+      then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          this.qrScanner.show();
+          this.scanSub = document.getElementsByTagName('body')[0].style.opacity = '0';
+          debugger
+          this.scanSub = this.qrScanner.scan()
+            .subscribe((textFound: string) => {
+              document.getElementsByTagName('body')[0].style.opacity = '1';
+              this.qrScanner.hide();
+              this.scanSub.unsubscribe();
+
+              this.qrText = textFound;
+            }, (err) => {
+              alert(JSON.stringify(err));
+            });
+
+        } else if (status.denied) {
+        } else {
+
+        }
+      })
+      .catch((e: any) => console.log('Error is', e));
+  }
+
 }
